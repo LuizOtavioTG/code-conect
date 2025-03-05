c4caae2fd1fdca82a373bdee0c533fcf891a3e1d
monica hillman <mmhillman1@ucs.br>
2025-03-05 10:32:35 -0300

    fix error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { RegistroComponent } from './paginas/registro/registro.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RegistroComponent],
+  imports: [ RegistroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
